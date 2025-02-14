d28d8599922de693bb61c91294c71361da4f30e2
cable <bitcoinkong2020@gmail.com>
2025-02-14 11:45:46 +0700

    Update server3.js

--- final_2/server3.js
+++ final_2/server3.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const productsController2 = require("./controllers/prod2");
 const productsController = require("./controllers/productsController");
 const usersController = require("./controllers/usersController");
 const authController =require("./controllers/authController")
@@ -18,7 +17,6 @@ const authController =require("./controllers/authController")
 const port = 3001;
 
 app.use("/products", productsController);
-app.use("/products2", productsController2);
 app.use("/users", usersController);
 app.use("/auth", authController);
 
