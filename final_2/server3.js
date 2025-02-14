6c22485f2c9b61d4c14d52b9b5abb775b0b6ea40
cable <bitcoinkong2020@gmail.com>
2025-02-14 10:20:08 +0700

    init server

---
+++ final_2/server3.js
@@ -0,0 +1,32 @@
+
+require("dotenv").config();
+require("./middlewares/auth");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const productsController = require("./controllers/productsController");
+const usersController = require("./controllers/usersController");
+
+const port = 3001;
+
+app.use("/products", productsController);
+app.use("/users", usersController);
+
+
+app.get("/", async (req, res) => {
+  res.send({
+    message: "server is running ",
+    version: "1.0.1",
+    secret: process.env.jwt_secret,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at localhost:${port}`);
+});
