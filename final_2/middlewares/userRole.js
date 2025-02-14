ed763ce37cd5da871199b70407b7da31837c13e6
cable <bitcoinkong2020@gmail.com>
2025-02-14 11:08:59 +0700

    add auth jwt

---
+++ final_2/middlewares/userRole.js
@@ -0,0 +1,39 @@
+
+
+const isAdmin = (req,res,next) => {
+    const user = req.user
+    if(user.role == "admin"){
+        next();
+    }else{
+        return res.status(403).send({
+            message: "Forbidden: Admin role required."
+          });
+    }
+}
+
+const isUser = (req,res,next) => {
+    const user = req.user
+    if(user.role == "user"){
+        next
+    }else{
+        return res.status(403).send({
+            message: "Forbidden: User role only."
+          });
+    }
+}
+
+const UserOrAdmin = (req,res,next) => {
+    const user = req.user
+    if(user.role == "admin" || user.role == "user"){
+        next();
+    }else{
+        return res.status(403).send({
+            message: "Forbidden: Admin or User role required."
+          });
+    }
+}
+
+module.exports = {
+    isAdmin,
+    UserOrAdmin
+}
