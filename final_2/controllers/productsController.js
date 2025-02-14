f2fc7ed5a8152a9362712c5f47cd00a9ef55434e
Onewayik <151303914+Onewayik@users.noreply.github.com>
2025-02-14 11:01:50 +0700

    Update productsController.js

--- final_2/controllers/productsController.js
+++ final_2/controllers/productsController.js
@@ -31,3 +31,92 @@ router.get("/",
 router.get("")
 
 module.exports = router;
+//สามารถสร้าง product ได้ [admin]
+router.post("/add", 
+  
+  async (req, res) => {
+    try {
+
+      const { name, price, category } = req.body;
+  
+      const errorResult = validationResult(req);
+
+      if(!errorResult.isEmpty()){
+           return res.status(400).send({
+            message: "Validation error",
+            errors: errorResult.array(),
+          });
+      }
+  
+  
+      await client.connect();
+  
+      const db = client.db(mongoDbInstance.getDbName());
+      const collection = db.collection("products");
+  
+      const result = await collection.insertOne({ name, price, category });
+  
+      return res.status(201).send({
+        message: "Product added successfully.",
+        result,
+      });
+    } catch (error) {
+      return res.status(500).send({
+        message: "Error adding product.",
+        error: error.message || error,
+      });
+    } finally {
+      await client.close();
+    }
+  });
+  //สามารถแก้ไขข้้อมูลสินค้าได้ด้วย product id [admin]
+  router.put("/edit/:id", 
+
+    async (req, res) => {
+    try {
+  
+      const { id } = req.params;
+      const { name, price, category } = req.body;
+  
+      const errorResult = validationResult(req);
+  
+      if(!errorResult.isEmpty()){
+           return res.status(400).send({
+            message: "Validation error",
+            errors: errorResult.array(),
+          });
+      }
+  
+  
+      await client.connect();
+  
+      const db = client.db(mongoDbInstance.getDbName());
+      const collection = db.collection(collectionName);
+  
+      const updatedUser = await collection.updateOne(
+        { _id: new ObjectId(id) }, 
+        { $set: { name, price, category } }
+      );
+  
+      if (updatedUser.matchedCount === 0) {
+        return res.status(404).send({
+          message: "Product not found",
+        });
+      }
+  
+      return res.status(200).send({
+        message: "Product updated successfully",
+        updatedUser,
+      });
+    } catch (error) {
+      return res.status(500).send({
+        message: "Error updating Product",
+        error: error.message || error,
+      });
+    } finally {
+      await client.close();
+    }
+  });
+
+
+  
