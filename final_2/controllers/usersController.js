70831550c5034667f90442e2ed6505814bf05e16
Onewayik <151303914+Onewayik@users.noreply.github.com>
2025-02-14 10:41:42 +0700

    Revert "Merge branch 'SetUp4Test' of https://github.com/CableKungZ/coe-backendPrograming into SetUp4Test"
    This reverts commit 0f9b3568ce5b6770d809fbd8eb894896732f84e7, reversing
    changes made to 3c444e4528b57c6f820e47d7ad40b690a55a2252.

--- final_2/controllers/usersController.js
+++ final_2/controllers/usersController.js
@@ -13,66 +13,186 @@ const client = mongoDbInstant.getMongoClient();
 const collectionName = "users";
 
 const saltRounds = 10;
+const jwtAuth = passport.authenticate("jwt-verify", { session: false });
 
+// Get all users only admin
 router.get("/",
-    async (req, res) => {
-    try {
-      await client.connect();
-      const db = client.db(mongoDbInstant.getDbName());
-      const collection = db.collection(collectionName);
-  
-      const users = await collection.find().toArray();
-  
-      return res.send(users);
-    } catch (error) {
-      return res.status(500).send({
-        message: "Error fetching users",
-        error,
+  jwtAuth,
+  middleware.isAdmin,
+  async (req, res) => {
+  try {
+    const errorResult = validationResult(req);
+
+    if(!errorResult.isEmpty()){
+         return res.status(400).send({
+          message: "Validation errorrrrr",
+          errors: errorResult.array(),
+        });
+    }
+
+
+    
+
+    await client.connect();
+
+    const db = client.db(mongoDbInstant.getDbName());
+    const collection = db.collection(collectionName);
+
+    const users = await collection.find().toArray();
+
+    return res.send(users);
+  } catch (error) {
+    return res.status(500).send({
+      message: "Error fetching users",
+      error,
+    });
+  } finally {
+    await client.close();
+  }
+});
+
+// Add user
+router.post("/add",
+  jwtAuth,
+  middleware.isAdmin,
+  validator.createUser,
+  async (req, res) => {
+  try {
+    const { username, password, full_name, role } = req.body;
+
+    await client.connect();
+
+    const db = client.db(mongoDbInstant.getDbName());
+    const collection = db.collection(collectionName);
+
+    const userExists = await collection.countDocuments({ username });
+    if (userExists > 0) {
+      return res.status(400).send({
+        message: "User already exists",
       });
-    } finally {
-      await client.close();
     }
-  });
 
-router.post("/create", async (req,res) => {
-    try {
-        const { username , password , full_name , role } = req.body;
-        await client.connect();
-        const db = client.db(mongoDbInstant.getDbName());
-        const collection = db.collection(collectionName);
-        // check user
-        const userExists = await collection.countDocuments({ username });
-        if (userExists > 0) {
-          return res.status(400).send({
-            message: "User already exists",
-          });
-        }
-        const passwordHash = bcrypt.hashSync(password, saltRounds);
+    const passwordHash = bcrypt.hashSync(password, saltRounds);
 
-        const user = {
-            username,
-            password_hash: passwordHash,
-            full_name,
-            role,
-          };
-        
-        const result = await collection.insertOne(user);
-        return res.status(200).send({
-            message: "User created successfully",
-            result,
-          });
+    const user = {
+      username,
+      password_hash: passwordHash,
+      full_name,
+      role,
+    };
 
+    const result = await collection.insertOne(user);
 
-    } catch (error) {
-        
-        return res.status(500).send({
-            message: "Error creating user",
-            error,
-          });
+    return res.status(200).send({
+      message: "User created successfully",
+      result,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      message: "Error creating user",
+      error,
+    });
+  } finally {
+    await client.close();
+  }
+});
 
-    } finally {
-        await client.close()
+// Update user
+router.put("/update/:id", 
+  jwtAuth,
+  middleware.isAdmin,
+  validator.updateUser,
+  async (req, res) => {
+  try {
+/*     if (req.user.role !== "admin") {
+      return res.status(403).send({
+        message: "Forbidden: Admin role required."
+      });
+    } */
+
+    const { id } = req.params;
+    const { username, full_name, role } = req.body;
+
+    const errorResult = validationResult(req);
+
+    if(!errorResult.isEmpty()){
+         return res.status(400).send({
+          message: "Validation error",
+          errors: errorResult.array(),
+        });
     }
-})
+
+
+    await client.connect();
+
+    const db = client.db(mongoDbInstant.getDbName());
+    const collection = db.collection(collectionName);
+
+    const updatedUser = await collection.updateOne(
+      { _id: new ObjectId(id) }, 
+      { $set: { username, full_name, role } }
+    );
+
+    if (updatedUser.matchedCount === 0) {
+      return res.status(404).send({
+        message: "User not found",
+      });
+    }
+
+    return res.status(200).send({
+      message: "User updated successfully",
+      updatedUser,
+    });
+  } 
+    catch (error) {
+      console.error("Error updating user:", error);
+      return res.status(500).send({
+        message: "Error updating user",
+        error: error.message || error, // Include meaningful error information
+      });
+    }
+    
+
+finally {
+    await client.close();
+  }
+});
+
+// Delete user
+router.delete("/delete/:id", jwtAuth, async (req, res) => {
+  try {
+/*     if (req.user.role !== "admin") {
+      return res.status(403).send({
+        message: "Forbidden: Admin role required."
+      });
+    }
+ */
+    const { id } = req.params;
+
+    await client.connect();
+
+    const db = client.db(mongoDbInstant.getDbName());
+    const collection = db.collection(collectionName);
+
+    const deletedUser = await collection.deleteOne({ _id: new mongoDbInstant.ObjectId(id) });
+
+    if (deletedUser.deletedCount === 0) {
+      return res.status(404).send({
+        message: "User not found",
+      });
+    }
+
+    return res.status(200).send({
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).send({
+      message: "Error deleting user",
+      error,
+    });
+  } finally {
+    await client.close();
+  }
+});
 
 module.exports = router;
