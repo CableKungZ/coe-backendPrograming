5d75ac414a6b6a9a6cc2cd454713b098cf54de96
cable <bitcoinkong2020@gmail.com>
2025-02-14 11:21:44 +0700

    add prod2

--- final_2/server3.js
+++ final_2/server3.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const productsController2 = require("./controllers/prod2");
 const productsController = require("./controllers/productsController");
 const usersController = require("./controllers/usersController");
 const authController =require("./controllers/authController")
@@ -17,6 +18,7 @@ const authController =require("./controllers/authController")
 const port = 3001;
 
 app.use("/products", productsController);
+app.use("/products2", productsController);
 app.use("/users", usersController);
 app.use("/auth", authController);
 
