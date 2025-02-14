f99b83229a32c93316b99859529bdb71a9cf4ac7
cable <bitcoinkong2020@gmail.com>
2025-02-14 10:52:36 +0700

    del

--- final_2/controllers/usersController.js
+++ final_2/controllers/usersController.js
@@ -10,36 +10,36 @@ const saltRounds = 10;
 
 router.get("/",
     async (req, res) => {
-    try {
-      await client.connect();
-      const db = client.db(mongoDbInstant.getDbName());
-      const collection = db.collection(collectionName);
-  
-      const users = await collection.find().toArray();
-  
-      return res.send(users);
-    } catch (error) {
-      return res.status(500).send({
-        message: "Error fetching users",
-        error,
-      });
-    } finally {
-      await client.close();
-    }
-  });
+        try {
+            await client.connect();
+            const db = client.db(mongoDbInstant.getDbName());
+            const collection = db.collection(collectionName);
 
-router.post("/create", async (req,res) => {
+            const users = await collection.find().toArray();
+
+            return res.send(users);
+        } catch (error) {
+            return res.status(500).send({
+                message: "Error fetching users",
+                error,
+            });
+        } finally {
+            await client.close();
+        }
+    });
+
+router.post("/create", async (req, res) => {
     try {
-        const { username , password , full_name , role } = req.body;
+        const { username, password, full_name, role } = req.body;
         await client.connect();
         const db = client.db(mongoDbInstant.getDbName());
         const collection = db.collection(collectionName);
         // check user
         const userExists = await collection.countDocuments({ username });
         if (userExists > 0) {
-          return res.status(400).send({
-            message: "User already exists",
-          });
+            return res.status(400).send({
+                message: "User already exists",
+            });
         }
         const passwordHash = bcrypt.hashSync(password, saltRounds);
 
@@ -48,61 +48,93 @@ router.post("/create", async (req,res) => {
             password_hash: passwordHash,
             full_name,
             role,
-          };
-        
+        };
+
         const result = await collection.insertOne(user);
         return res.status(200).send({
             message: "User created successfully",
             result,
-          });
+        });
 
 
     } catch (error) {
-        
+
         return res.status(500).send({
             message: "Error creating user",
             error,
-          });
+        });
 
     } finally {
         await client.close()
     }
 })
 
-router.put("/update/:id", async (req,res) => {
+router.put("/update/:id", async (req, res) => {
     try {
-    const { id } = req.params;
-    const { username, full_name, role } = req.body;
-        
-    await client.connect();
+        const { id } = req.params;
+        const { username, full_name, role } = req.body;
 
-    const db = client.db(mongoDbInstant.getDbName());
-    const collection = db.collection(collectionName);
+        await client.connect();
 
-    const updatedUser = await collection.updateOne(
-      { _id: new ObjectId(id) }, 
-      { $set: { username, full_name, role } }
-    );
-    if (updatedUser.matchedCount === 0) {
-        return res.status(404).send({
-          message: "User not found",
+        const db = client.db(mongoDbInstant.getDbName());
+        const collection = db.collection(collectionName);
+
+        const updatedUser = await collection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set: { username, full_name, role } }
+        );
+        if (updatedUser.matchedCount === 0) {
+            return res.status(404).send({
+                message: "User not found",
+            });
+        }
+        return res.status(200).send({
+            message: "User updated successfully",
+            updatedUser,
         });
-      }
-      return res.status(200).send({
-        message: "User updated successfully",
-        updatedUser,
-      });
 
     } catch (error) {
         console.error("Error updating user:", error);
         return res.status(500).send({
-          message: "Error updating user",
-          error: error.message || error, // Include meaningful error information
+            message: "Error updating user",
+            error: error.message || error, // Include meaningful error information
         });
     } finally {
         await client.close();
     }
 })
 
+router.delete("/delete/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        await client.connect();
+
+        const db = client.db(mongoDbInstant.getDbName());
+        const collection = db.collection(collectionName);
+
+        const deletedUser = await collection.deleteOne({ _id: new mongoDbInstant.ObjectId(id) });
+
+        if (deletedUser.deletedCount === 0) {
+            return res.status(404).send({
+                message: "User not found",
+            });
+        }
+
+
+        return res.status(200).send({
+            message: "User deleted successfully",
+        });
+
+    } catch (error) {
+
+        return res.status(500).send({
+            message: "Error deleting user",
+            error,
+        });
+
+    } finally {
+        await client.close();
+    }
+})
 
 module.exports = router;
