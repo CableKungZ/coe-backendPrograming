a0e32496687b366c34872a592ecb7c38d5ecd2a0
cable <bitcoinkong2020@gmail.com>
2025-02-14 10:35:06 +0700

    Update usersController.js

--- final_2/controllers/usersController.js
+++ final_2/controllers/usersController.js
@@ -28,4 +28,45 @@ router.get("/",
     }
   });
 
+router.post("/create", async (req,res) => {
+    try {
+        const { username , password , full_name , role } = req.body;
+        await client.connect();
+        const db = client.db(mongoDbInstant.getDbName());
+        const collection = db.collection(collectionName);
+        // check user
+        const userExists = await collection.countDocuments({ username });
+        if (userExists > 0) {
+          return res.status(400).send({
+            message: "User already exists",
+          });
+        }
+        const passwordHash = bcrypt.hashSync(password, saltRounds);
+
+        const user = {
+            username,
+            password_hash: passwordHash,
+            full_name,
+            role,
+          };
+        
+        const result = await collection.insertOne(user);
+        return res.status(200).send({
+            message: "User created successfully",
+            result,
+          });
+
+
+    } catch (error) {
+        
+        return res.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+
+    } finally {
+        await client.close()
+    }
+})
+
 module.exports = router;
